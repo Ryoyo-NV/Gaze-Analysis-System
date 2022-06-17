28d4896872513efaffc4efba01e088485e1d3d95
takutoi <takuto_ishii@ryoyo.co.jp>
2022-06-17 13:45:56 +0900

    fix #9

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ echo
 
 # INSTALL DEPENDENCIES
 echo Installing dependencies...
+sudo apt update
 sudo apt install -y gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
 	gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer1.0-dev\
 	libgstreamer-plugins-base1.0-dev   libgstreamer-plugins-good1.0-dev   libgstreamer-plugins-bad1.0-dev\
